b1bec2c57a64d31d73b6225192247f73fa88c8ca
talon <lily@hzcwtech.com>
2017-10-18 16:57:16 +0800

    Initial commit

---
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice14MaskFilterView.java
@@ -0,0 +1,64 @@
+package com.hencoder.hencoderpracticedraw2.practice;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BlurMaskFilter;
+import android.graphics.Canvas;
+import android.graphics.MaskFilter;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.hencoder.hencoderpracticedraw2.R;
+
+public class Practice14MaskFilterView extends View {
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Bitmap bitmap;
+    MaskFilter filter1 = new BlurMaskFilter(10, BlurMaskFilter.Blur.NORMAL);
+    MaskFilter filter2 = new BlurMaskFilter(10, BlurMaskFilter.Blur.INNER);
+    MaskFilter filter3 = new BlurMaskFilter(10, BlurMaskFilter.Blur.OUTER);
+    MaskFilter filter4 = new BlurMaskFilter(10, BlurMaskFilter.Blur.SOLID);
+
+    public Practice14MaskFilterView(Context context) {
+        super(context);
+    }
+
+    public Practice14MaskFilterView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Practice14MaskFilterView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    {
+        setLayerType(LAYER_TYPE_SOFTWARE, null);
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.what_the_fuck);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // 用 Paint.setMaskFilter 来设置不同的 BlurMaskFilter
+
+        paint.setMaskFilter(filter1);
+        // 第一个：NORMAL
+        canvas.drawBitmap(bitmap, 100, 50, paint);
+
+        // 第二个：INNER
+        paint.setMaskFilter(filter2);
+        canvas.drawBitmap(bitmap, bitmap.getWidth() + 200, 50, paint);
+
+        // 第三个：OUTER
+        paint.setMaskFilter(filter3);
+        canvas.drawBitmap(bitmap, 100, bitmap.getHeight() + 100, paint);
+
+        // 第四个：SOLID
+        paint.setMaskFilter(filter4);
+        canvas.drawBitmap(bitmap, bitmap.getWidth() + 200, bitmap.getHeight() + 100, paint);
+    }
+}
